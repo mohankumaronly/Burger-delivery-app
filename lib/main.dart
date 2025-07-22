5481601668ba0e90e6c94c6ef1e7a8a3da477fa9
Mohan <mohankumaronly81@gmail.com>
2025-07-22 16:53:16 +0530

    Files are arranged

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:food_delivery_app/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,16 +6,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: SplashScreen(),
+      title: 'Food Delivery application Learning',
+      home: Scaffold(),
     );
   }
 }
